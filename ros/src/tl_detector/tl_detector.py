5a557da96c92c065f4638001d86e7b07a5aa4c96
tos36 <toshiyuki.kaji36@gmail.com>
2019-09-28 10:44:51 +0000

    bug fix tl_ditector.py

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -141,7 +141,7 @@ class TLDetector(object):
         # List of positions that correspond to the line to stop in front of for a given intersection
         stop_line_positions = self.config['stop_line_positions']
         if(self.pose):
-            car_position = self.get_closest_waypoint(self.pose.pose)
+            car_wp_idx  = self.get_closest_waypoint(self.pose.pose.position.x, self.pose.pose.position.y)
 
             #TODO find the closest visible traffic light (if one exists)
             diff = len(self.waypoints.waypoints)
@@ -158,7 +158,8 @@ class TLDetector(object):
         if closest_light:
             state = self.get_light_state(closest_light)
             return light_wp_idx, state
-        self.waypoints = None
+        
+        #self.waypoints = None
         return -1, TrafficLight.UNKNOWN
 
 if __name__ == '__main__':
