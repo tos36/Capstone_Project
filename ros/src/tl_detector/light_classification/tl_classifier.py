6b7c8919f7ca1a3dd1c182f828f788489c8825c8
tos36 <toshiyuki.kaji@gmail.com>
2019-10-01 09:03:48 +0000

    add light cllasification module

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,27 @@
 from styx_msgs.msg import TrafficLight
+import tensorflow as tf
+import numpy as np
 
 class TLClassifier(object):
     def __init__(self):
         #TODO load classifier
-        pass
+        PATH = r'light_classification/model/ssd/graph.pb'
+        self.graph = tf.Graph()
+        self.thresh = 0.5
+        
+        with self.graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(PATH, 'rb') as fid:
+                od_graph_def.ParseFromString(fid.read())
+                tf.import_graph_def(od_graph_def, name='')
+
+            self.image_tensor = self.graph.get_tensor_by_name('image_tensor:0')
+            self.boxes = self.graph.get_tensor_by_name('detection_boxes:0')
+            self.scores = self.graph.get_tensor_by_name('detection_scores:0')
+            self.classes = self.graph.get_tensor_by_name('detection_classes:0')
+            self.num_detections = self.graph.get_tensor_by_name('num_detections:0')
+
+            self.sess = tf.Session(graph=self.graph)
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -16,4 +34,26 @@ class TLClassifier(object):
 
         """
         #TODO implement light color prediction
-        return TrafficLight.GREEN
+        #return TrafficLight.RED
+        with self.graph.as_default():
+            img_exp = np.expand_dims(image, axis=0)
+            (boxes, scores, classes, num_detections) = self.sess.run(
+                [self.boxes, self.scores, self.classes, self.num_detections],
+                feed_dict={self.image_tensor: img_exp})
+
+        boxes = np.squeeze(boxes)
+        scores = np.squeeze(scores)
+        classes = np.squeeze(classes).astype(np.int32)
+
+        if scores[0] > self.thresh:
+            if classes[0] == 1:
+                #print('GREEN')
+                return TrafficLight.GREEN
+            elif classes[0] == 2:
+                #print('RED')
+                return TrafficLight.RED
+            elif classes[0] == 3:
+                #print('YELLOW')
+                return TrafficLight.YELLOW
+
+        return TrafficLight.UNKNOWN
